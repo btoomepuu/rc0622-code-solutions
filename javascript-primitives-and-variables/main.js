2914ff496da15a590409e203482e5abf84f191e6
Brennon Toomepuu <btoomepuu@gmail.com>
2022-07-05 17:43:41 +0000

    completed  javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,13 @@
+var fullName = 'Brennon Toomepuu';
+var isCool = false;
+var totalPets = 4;
+var unknown = null;
+var never;
+
+console.log('value of full name', fullName);
+console.log('value of Is cool', isCool);
+console.log('value of Total Pets', totalPets);
+console.log('value of unknown', unknown);
+console.log('typeof unknown', typeof (unknown));
+console.log('value of never', never);
+console.log('typeof never', never);
