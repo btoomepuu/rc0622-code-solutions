5a4187c9cc23b02288e2c3706b26fc7f71e1311e
Brennon Toomepuu <btoomepuu@gmail.com>
2022-07-06 01:28:07 +0000

    complete javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,58 @@
+var orderHistory = [
+  {
+    numberItems: 1,
+    customer: 'JS Masher',
+    orderDate: 'August 4, 2020',
+    orderNumber: '114-3941689-8772232',
+    orderDelivered: 'Aug 8, 2020',
+    price: 31.55,
+    total: 34.00,
+    type: 'book',
+    title: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnBy: 'Sept 7, 2020'
+  },
+  {
+    numberItems: 1,
+    customer: 'JS Masher',
+    orderDate: 'July 19, 2020',
+    orderNumber: '113-994268-1280257',
+    orderDelivered: 'Jul 20, 2020',
+    price: 41.33,
+    total: 45.53,
+    type: 'book',
+    title: 'The Timeless WAy of Building',
+    author: 'Alexander, Christopher',
+    returnBy: 'Aug 19, 2020'
+  },
+  {
+    numberItems: 1,
+    customer: 'JS Masher',
+    orderDate: 'July 4, 2020',
+    orderNumber: '114-2875557-9059409',
+    orderDelivered: 'Jul 2, 2020',
+    price: 15.98,
+    total: 17.22,
+    type: 'gaming',
+    title: 'Gamecube Controller Adapter. Super Smash Bros Swotch Gambecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: 'none',
+    returnBy: 'Aug 5, 2020'
+  },
+  {
+    numberItems: 2,
+    customer: 'JS Masher',
+    orderDate: 'July 3, 2020',
+    orderNumber: '113-2883177-2648248',
+    orderDelivered: 'Jul 5, 2020',
+    price1: 94.95,
+    price2: 33.99,
+    type1: 'gaming',
+    type2: 'book',
+    title1: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)',
+    title2: 'The Art od SQL',
+    author1: 'none',
+    author2: 'Faroult, Stephane',
+    total: 138.93,
+    returnBy: 'Aug 4, 2020'
+  }
+];
