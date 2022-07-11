3cd25ffd6744e9bf8ea57259d2e2a8becece7059
Brennon Toomepuu <btoomepuu@gmail.com>
2022-07-11 18:19:30 +0000

    completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+const elementSelect = document.querySelector('h1');
+console.log(elementSelect);
+console.dir(elementSelect);
+
+const idSelect = document.querySelector('#explanation');
+console.log(idSelect);
+console.dir(idSelect);
+
+const classSelect = document.querySelector('.hint');
+console.log(classSelect);
+console.dir(classSelect);
+
+const elementALLSelect = document.querySelectorAll('p');
+console.log(elementALLSelect);
+
+const classSelectAll = document.querySelectorAll('.example-link');
+console.log(classSelectAll);
