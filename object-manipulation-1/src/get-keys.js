40d90be79154bbc68d245e021107c83b1afe33ac
Brennon Toomepuu <btoomepuu@gmail.com>
2022-07-11 14:22:39 +0000

    clompeted object-manipulation-1

---
+++ object-manipulation-1/src/get-keys.js
@@ -0,0 +1,17 @@
+/* exported getKeys */
+/*
+input: object
+output: array with given objects keys
+-create empty array
+-use for in to access keys in the object
+  push keys to array
+-return array
+*/
+
+function getKeys(object) {
+  const keysArray = [];
+  for (const keys in object) {
+    keysArray.push(keys);
+  }
+  return keysArray;
+}
