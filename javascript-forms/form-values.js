c1b5fa7967d1297042b3f68f7c9b9c52abf3d9e1
Brennon Toomepuu <btoomepuu@gmail.com>
2022-07-13 18:55:20 +0000

    completed  javascript-forms

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,13 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  var elements = document.getElementById('contact-form').elements;
+  var formValues = {};
+  for (var i = 0; i < elements.length; i++) {
+    var item = elements.item(i);
+    formValues[item.name] = item.value;
+  }
+  console.log('formValues:', formValues);
+}
+
+var $contactForm = document.querySelector('#contact-form');
+$contactForm.addEventListener('submit', handleSubmit);
