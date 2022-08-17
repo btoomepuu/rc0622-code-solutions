151325971aa15daef03e9220f568991fafe3702c
Brennon Toomepuu <btoomepuu@gmail.com>
2022-08-17 17:14:44 +0000

    updated es6-arrow-functions

--- es6-arrow-functions/main.js
+++ es6-arrow-functions/main.js
@@ -5,41 +5,38 @@ const $jokeForm = document.querySelector('#joke-form');
 const jokester = {
   tellJoke: function (setup, punchline) {
     $jokeForm.classList.add('d-none');
-    const $introStatement = renderJokePhrase('Hey Flash...');
-    appendJokePhrase($introStatement);
-    setTimeout(function () {
-      const $jokeSetup = renderJokePhrase(setup);
-      appendJokePhrase($jokeSetup);
-      setTimeout(function () {
-        const $jokePunchline = renderJokePhrase(punchline);
-        appendJokePhrase($jokePunchline);
-        flash();
+    const $introStatement = this.renderJokePhrase('Hey Flash...');
+    this.appendJokePhrase($introStatement);
+    setTimeout(() => {
+      const $jokeSetup = this.renderJokePhrase(setup);
+      this.appendJokePhrase($jokeSetup);
+      setTimeout(() => {
+        const $jokePunchline = this.renderJokePhrase(punchline);
+        this.appendJokePhrase($jokePunchline);
+        flash.laugh();
       }, 2000);
     }, 2000);
+  },
+  renderJokePhrase: function (phrase) {
+    const $phrase = document.createElement('h4');
+    $phrase.textContent = phrase;
+    $phrase.className = 'fade-in text-center mb-3 col-12';
+    return $phrase;
+  },
+  appendJokePhrase: function ($phrase) {
+    $jokeContainer.append($phrase);
   }
 };
 
-const renderJokePhrase = phrase => {
-  const $phrase = document.createElement('h4');
-  $phrase.textContent = phrase;
-  $phrase.className = 'fade-in text-center mb-3 col-12';
-  return $phrase;
+const flash = {
+  laughingUrl: 'images/flash-laugh.gif',
+  laugh: function () {
+    setTimeout(() => {
+      $flashImage.setAttribute('src', this.laughingUrl);
+    }, 3000);
+  }
 };
 
-const appendJokePhrase = $phrase => {
-  $jokeContainer.append($phrase);
-};
-
-const flash = () => {
-  const laughingUrl = 'images/flash-laugh.gif';
-  laugh(laughingUrl);
-};
-
-const laugh = url => {
-  setTimeout(function () {
-    $flashImage.setAttribute('src', url);
-  }, 3000);
-};
 function handleJokeSubmission(event) {
   event.preventDefault();
   const { setup, punchline } = event.target.elements;
