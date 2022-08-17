87a9ef8f4e709944733960bad4bf78d2fb8b71ff
Brennon Toomepuu <btoomepuu@gmail.com>
2022-08-17 19:54:04 +0000

    completed  node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('type of export', typeof module.exports);
+console.log('export value:', module.exports);
+
+console.log('require type', typeof require.main);
+console.log('require value:', require.main);
+
+console.log('type of module', typeof module);
+console.log('module value:', module);
+
+console.log('type of filename', typeof __filename);
+console.log('filename value:', __filename);
+
+console.log('type directory', __dirname);
+console.log('directory value:', __dirname);
